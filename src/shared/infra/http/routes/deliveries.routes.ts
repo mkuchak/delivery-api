85161607322ac90fb415c4cf258b86d420984c96
Marcos Kuchak <mkuchak@hotmail.com>
2022-03-23 15:34:03 -0300

    feat: add routes to deliverymen manage deliveries

--- src/shared/infra/http/routes/deliveries.routes.ts
+++ src/shared/infra/http/routes/deliveries.routes.ts
@@ -1,13 +1,19 @@
 import { Router } from 'express'
 
+import { AcceptDeliveryController } from '@/modules/deliveries/useCases/acceptDelivery/AcceptDeliveryController'
+import { CancelDeliveryController } from '@/modules/deliveries/useCases/cancelDelivery/CancelDeliveryController'
 import { CreateDeliveryController } from '@/modules/deliveries/useCases/createDelivery/CreateDeliveryController'
 import { FindOpenDeliveriesController } from '@/modules/deliveries/useCases/findOpenDeliveries/FindOpenDeliveriesController'
+import { FinishDeliveryController } from '@/modules/deliveries/useCases/finishDelivery/FinishDeliveryController'
 
 import { ensureCustomerAuthenticated } from '../middlewares/ensureCustomerAuthenticated'
 import { ensureDeliverymanAuthenticated } from '../middlewares/ensureDeliverymanAuthenticated'
 
 const createDeliveryController = new CreateDeliveryController()
 const findOpenDeliveriesController = new FindOpenDeliveriesController()
+const acceptDeliveryController = new AcceptDeliveryController()
+const finishDeliveryController = new FinishDeliveryController()
+const cancelDeliveryController = new CancelDeliveryController()
 
 const deliveriesRoutes = Router()
 
@@ -20,6 +26,21 @@ deliveriesRoutes.get(
   '/',
   ensureDeliverymanAuthenticated,
   findOpenDeliveriesController.handle,
+)
+deliveriesRoutes.patch(
+  '/:deliveryId/accept',
+  ensureDeliverymanAuthenticated,
+  acceptDeliveryController.handle,
+)
+deliveriesRoutes.patch(
+  '/:deliveryId/finish',
+  ensureDeliverymanAuthenticated,
+  finishDeliveryController.handle,
+)
+deliveriesRoutes.patch(
+  '/:deliveryId/cancel',
+  ensureDeliverymanAuthenticated,
+  cancelDeliveryController.handle,
 )
 
 export { deliveriesRoutes }
