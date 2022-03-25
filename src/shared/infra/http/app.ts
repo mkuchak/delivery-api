f8bc6da444ed3825aa246b36d138e0b11837977f
Marcos Kuchak <mkuchak@hotmail.com>
2022-03-25 16:10:08 -0300

    chore: add cors and helmet

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -1,7 +1,9 @@
 import 'express-async-errors'
 import 'dotenv/config'
 
+import cors from 'cors'
 import express, { json } from 'express'
+import helmet from 'helmet'
 
 import { handleErrors } from './middlewares/handleErrors'
 import { router } from './routes'
@@ -9,6 +11,8 @@ import { router } from './routes'
 const app = express()
 
 app.use(json())
+app.use(cors())
+app.use(helmet())
 
 app.use(router)
 
